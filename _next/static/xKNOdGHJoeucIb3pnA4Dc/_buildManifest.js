99fe768b4896f62f7f3f89c6f1f6da70311c3cb1
riteshelias <ritesh.elias@gmail.com>
2026-04-01 19:37:57 +0530

    Updates

--- _next/static/xKNOdGHJoeucIb3pnA4Dc/_buildManifest.js
+++ _next/static/xKNOdGHJoeucIb3pnA4Dc/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/supplierdashboard//_next/:path+",
+        "destination": "/supplierdashboard/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
